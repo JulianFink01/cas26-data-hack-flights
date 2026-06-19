9f3224273287a779669270a35b8aa86d191cc8a6
JulianFink01 <JulianFink01>
2026-06-19 09:42:53 +0200

    display data

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def load_dataset() -> pd.DataFrame:
+    dataset = pd.read_csv("./data/flights.csv", low_memory=False)
+    return dataset
+
+def describe_dataset(dataset: pd.DataFrame):
+    print(dataset.describe())
+    print("-----")
+    print(dataset.head())
+
+def plot_dataset(dataset: pd.DataFrame) -> None:
+    sns.pairplot(dataset)
+    plt.show()
 
 def main():
-    pass
-
+    ds = load_dataset()
+    describe_dataset(ds)
+    plot_dataset(ds)
 
 if __name__ == "__main__":
     main()
